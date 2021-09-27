0a6af8fcf1481b020db8017c4ec3a83a4353d883
Полина Елатонцева <polya.elatonceva@mail.ru>
2021-09-28 00:02:58 +0300

    Основная версия программы с комментариями

--- LaboratoryWorks/Лабораторная работа 3/3.c
+++ LaboratoryWorks/Лабораторная работа 3/3.c
@@ -2,10 +2,14 @@
 
 int main(int argc, const char * argv[]) {
     float a, b;
+    
     printf("Введите а\n");
     scanf("%f", &a); //ввод с клавиатуры числа а
+    
     printf("Введите b\n");
     scanf("%f", &b); //ввод с клавиатуры числа b
+    
     printf("%f\n", (a + b) / 2); //подсчет результата и вывод его на экран
+    
     return 0;
 }
