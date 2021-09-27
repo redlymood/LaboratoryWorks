69fc86504c3b0b391f69ce4b03649fbb0dd6229e
Полина Елатонцева <polya.elatonceva@mail.ru>
2021-09-27 23:44:52 +0300

    Main version "LaboratoryWork3.c"

---
+++ LaboratoryWorks/LaboratoryWork3.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LaboratoryWorks
+//
+//  Created by Елатонцева Полина on 27.09.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
